ef9196e4c96a796702763d05ea2ea24d5c7685e2
3q1u <3qiu@desu.life>
2023-02-23 23:01:15 +0800

    3865 OOP accepted

--- WIP/P3865.cpp
+++ WIP/P3865.cpp
@@ -1,14 +1,20 @@
+#include <algorithm>
 #include <cstdio>
+#include <cstring>
 #include <cctype>
+#include <vector>
 
 using std::fread;
 using std::printf;
 using std::isdigit;
+using std::vector;
+
+const int MAXN = 1e5 + 5, MAXM = 2 * 1e6 + 5;
 
 struct FastIn
 {
 
-    static char buf[1<<21];
+    char buf[1<<21];
     char *p1 = buf, *p2 = buf;
     
     char getchar()
@@ -18,7 +24,7 @@ struct FastIn
         return *(p1++);
     }
 
-    template <typename T>
+    template <typename T = int>
     T operator () ()
     {
         T s = 0, f = 1;
@@ -37,3 +43,58 @@ struct FastIn
         return s * f;
     }
 } read;
+
+class STable
+{
+    public:
+
+        int n;
+        int dp[MAXN][20];
+        int log[MAXN];
+
+        STable() : STable(0) {}
+
+        STable(const int& n) : n(n)
+        {
+            memset(dp, 0, sizeof dp);
+            memset(log, 0, sizeof log);
+
+            log[1] = 0;
+            for(int i = 2; i <= n; i++)
+                log[i] = log[i >> 1] + 1;
+        }
+
+        void work(int container[])
+        {
+            for(int i = 1; i <= n; i++)
+                dp[i][0] = container[i];
+
+            for(int j = 1;j <= log[n]; j++)
+            for(int i = 1;i <= n - (1 << j) + 1; i++)
+                dp[i][j] = std::max(dp[i][j - 1], dp[i + (1 << (j - 1))][j - 1]);
+        }
+
+        int query(int k, int r)
+        {
+            int l = log[r - k + 1];
+            return std::max(dp[k][l], dp[r - (1 << l) + 1][l]);
+        }
+};
+
+int main()
+{
+    int a[MAXN];
+    int n = read(), m = read();
+    STable st(n);
+
+    for(int i = 1; i <= n; i++)
+        a[i] = read();
+    
+    st.work(a);
+
+    while(m--)
+    {
+        int l = read(), r = read();
+        printf("%d\n", st.query(l, r));
+    }
+}
